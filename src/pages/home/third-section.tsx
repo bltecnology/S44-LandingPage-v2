7c57c99325d10b23e693e67f6d6005b6b3b9d44e
leviutima <levi.utima@gmail.com>
2025-04-12 07:57:55 -0300

    fix display flex in list third section

--- src/pages/home/third-section.tsx
+++ src/pages/home/third-section.tsx
@@ -6,10 +6,7 @@ import { Check } from "lucide-react";
 export function ThirdSection() {
   return (
     <section className="third-section">
-      <div
-        className="bg-section"
-        style={{ backgroundImage: `url(${bannerImage})` }}
-      >
+      <div className="bg-section" style={{ backgroundImage: `url(${bannerImage})` }}>
         <div className="text-content">
           <h3 className="highlight-box">
             O Que é Possível Transportar Conosco?
@@ -18,38 +15,43 @@ export function ThirdSection() {
           <ul className="cargo-list">
             <li>
               <strong>Cargas Secas:</strong>
+              <div className="container-sub-list">
               <span>
                 Produtos não perecíveis como materiais de construção e
                 eletrodomésticos.
               </span>
-              <Check size={16} />
+              <Check size={28} />
+              </div>
             </li>
             <li>
               <strong>Cargas Perigosas:</strong>
+              <div className="container-sub-list">
               <span>
                 Produtos químicos e inflamáveis que requerem cuidados especiais
               </span>
-              <Check size={16} />
+              <Check size={28} />
+              </div>
             </li>
             <li>
               <strong>Cargas de Grande Porte:</strong>
+              <div className="container-sub-list">
               <span>Maquinários pesados e Equipamentos Industriais</span>
-              <Check size={16} />
+              <Check size={28} />
+              </div>
             </li>
             <li>
               <strong>Cargas Fracionadas:</strong>
+              <div className="container-sub-list">
               <span>
                 Pequenas quantidades de mercadorias consolidadas para eficiência
               </span>
-              <Check size={16} />
+              <Check size={28} />
+              </div>
             </li>
           </ul>
         </div>
       </div>
-      <div
-        className="truck-image"
-        style={{ backgroundImage: `url(${truckImage})` }}
-      ></div>
+      <div className="truck-image" style={{ backgroundImage: `url(${truckImage})` }}></div>
     </section>
   );
 }
