b5b84f6d3686301141693cb617c9c53ab4e02850
leviutima <levi.utima@gmail.com>
2025-04-12 07:50:28 -0300

    fix: resolving third section

--- src/pages/home/third-section.tsx
+++ src/pages/home/third-section.tsx
@@ -5,47 +5,51 @@ import { Check } from "lucide-react";
 
 export function ThirdSection() {
   return (
-    <section
-      className="third-section"
-      style={{ backgroundImage: `url(${bannerImage})` }}
-    >
-      <div className="text-content">
-        <h3 className="highlight-box">O Que é Possível Transportar Conosco?</h3>
+    <section className="third-section">
+      <div
+        className="bg-section"
+        style={{ backgroundImage: `url(${bannerImage})` }}
+      >
+        <div className="text-content">
+          <h3 className="highlight-box">
+            O Que é Possível Transportar Conosco?
+          </h3>
 
-        <ul className="cargo-list">
-          <li>
-            <strong>Cargas Secas:</strong>
-            <span>
-              Produtos não perecíveis como materiais de construção e
-              eletrodomésticos.
-            </span>
-            <Check size={16} />
-          </li>
-          <li>
-            <strong>Cargas Perigosas:</strong>
-            <span>
-              Produtos químicos e inflamáveis que requerem cuidados especiais
-            </span>
-            <Check size={16} />
-          </li>
-          <li>
-            <strong>Cargas de Grande Porte:</strong>
-            <span>Maquinários pesados e Equipamentos Industriais</span>
-            <Check size={16} />
-          </li>
-          <li>
-            <strong>Cargas Fracionadas:</strong>
-            <span>
-              Pequenas quantidades de mercadorias consolidadas para eficiência
-            </span>
-            <Check size={16}  />
-          </li>
-        </ul>
+          <ul className="cargo-list">
+            <li>
+              <strong>Cargas Secas:</strong>
+              <span>
+                Produtos não perecíveis como materiais de construção e
+                eletrodomésticos.
+              </span>
+              <Check size={16} />
+            </li>
+            <li>
+              <strong>Cargas Perigosas:</strong>
+              <span>
+                Produtos químicos e inflamáveis que requerem cuidados especiais
+              </span>
+              <Check size={16} />
+            </li>
+            <li>
+              <strong>Cargas de Grande Porte:</strong>
+              <span>Maquinários pesados e Equipamentos Industriais</span>
+              <Check size={16} />
+            </li>
+            <li>
+              <strong>Cargas Fracionadas:</strong>
+              <span>
+                Pequenas quantidades de mercadorias consolidadas para eficiência
+              </span>
+              <Check size={16} />
+            </li>
+          </ul>
+        </div>
       </div>
-
-      <div className="image-container">
-        <img src={truckImage} alt="Traseira do caminhão" className="truck" />
-      </div>
+      <div
+        className="truck-image"
+        style={{ backgroundImage: `url(${truckImage})` }}
+      ></div>
     </section>
   );
 }
